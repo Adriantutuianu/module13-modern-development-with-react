e4c4a63082828489f2aebd0f5569bbce967ae2e3
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-13 19:55:39 +0000

    Hello dev task react

---
+++ 01-intro-react-lesson/00-react-practice/src/components/HelloDiv.jsx
@@ -0,0 +1,13 @@
+function HelloDiv() {
+  const name = "Adrian"
+  return <>
+  <h1>Hello! My name is {name}</h1>
+  <ul>
+    <li>a</li>
+    <li>b</li>
+    <li>c</li>
+    </ul>
+    </>
+}
+
+export default HelloDiv;
