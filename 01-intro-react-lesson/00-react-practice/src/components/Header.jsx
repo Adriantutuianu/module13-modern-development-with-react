3e4a0bd69921e318409c850a658e2daf21a382f4
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-14 18:32:20 +0000

    Day2 starter pack

--- 01-intro-react-lesson/00-react-practice/src/components/Header.jsx
+++ 01-intro-react-lesson/00-react-practice/src/components/Header.jsx
@@ -2,17 +2,16 @@ import "../styles/Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
-
 function Header() {
   const myStyles = {
     headerStyles: {
-      background: "green"
+      background: "green",
     },
     h1Style: {
-      color: "black"
-    }
-  }
-  
+      color: "black",
+    },
+  };
+
   return (
     <header className="header" style={myStyles.headerStyles}>
       <h1 style={myStyles.h1Style}>Welcome</h1>
