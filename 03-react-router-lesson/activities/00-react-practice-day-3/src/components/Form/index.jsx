b1f68a5d1b76c755afcb7f6e625e4121df55b253
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-15 19:08:38 +0000

    Task 1 - forms

--- 03-react-router-lesson/activities/00-react-practice-day-3/src/components/Form/index.jsx
+++ 03-react-router-lesson/activities/00-react-practice-day-3/src/components/Form/index.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
-import './style.css';
+import { useState } from "react";
+import "./style.css";
 
 function Form() {
   // Setting the component's initial state
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
+    firstName: "",
+    lastName: "",
+    password: "",
   });
 
   const handleInputChange = (event) => {
+    event.preventDefault();
     // Getting the value and name of the input which triggered the change
     const value = event.target.value;
     const name = event.target.name;
@@ -23,12 +25,18 @@ function Form() {
   const handleFormSubmit = (event) => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
-
+    if (formData.firstName || formData.lastName) {
+      alert("need both names");
+      return;
+    }
     // Alert the user their first and last name, clear `formData.firstName` and `formData.lastName`, clearing the inputs
-    alert(`Hello ${formData.firstName} ${formData.lastName}`);
+    alert(
+      `Hello ${formData.firstName} ${formData.lastName} ${formData.password}`
+    );
     setFormData({
-      firstName: '',
-      lastName: '',
+      firstName: "",
+      lastName: "",
+      password: "",
     });
   };
 
@@ -52,6 +60,13 @@ function Form() {
           onChange={handleInputChange}
           type="text"
           placeholder="Last Name"
+        />
+        <input
+          value={formData.password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
         />
         <button onClick={handleFormSubmit}>Submit</button>
       </form>
