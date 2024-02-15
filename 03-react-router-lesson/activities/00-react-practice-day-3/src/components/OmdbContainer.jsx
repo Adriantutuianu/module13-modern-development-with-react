1e76c5764397ff5c31e18b29d1d0249a7b517a7d
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-15 20:23:35 +0000

    Task axios

--- 03-react-router-lesson/activities/00-react-practice-day-3/src/components/OmdbContainer.jsx
+++ 03-react-router-lesson/activities/00-react-practice-day-3/src/components/OmdbContainer.jsx
@@ -1,30 +1,49 @@
-import { useState } from 'react';
-import Container from './Container';
-import Row from './Row';
-import Col from './Col';
-import Card from './Card';
-import SearchForm from './SearchForm';
-import MovieDetail from './MovieDetail';
-import API from '../utils/API';
+import { useState } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import SearchForm from "./SearchForm";
+import MovieDetail from "./MovieDetail";
+import API from "../utils/API";
 
 function OmdbContainer() {
   const [searchData, setSearchData] = useState({
-    search: '',
+    search: "",
     results: [],
   });
 
   // Add searchMovies function
-
+  const searchMovie = (searchTErm) => {
+    API.search(searchTErm)
+      .then((resp) => {
+        console.log(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   // Add handleInputChange function
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    let value = event.target.value;
 
+    setSearchData({
+      ...searchData,
+      [name]: value,
+    });
+  };
   // Add handleFormSubmit function
-
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    searchMovie(searchData.search);
+  };
   return (
     <Container>
       <Row>
         <Col size="md-8">
           <Card
-            heading={searchData.results.Title || 'Search for a Movie to Begin'}
+            heading={searchData.results.Title || "Search for a Movie to Begin"}
           >
             <MovieDetail
               title={searchData.results.Title}
